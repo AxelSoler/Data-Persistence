0cd0bc949268e75f488846d3564d810f9b3b4109
AxelSDev <axelss369@gmail.com>
2025-05-19 18:40:45 -0300

    Player Name

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -76,7 +76,7 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        ScoreText.text = $"Score: {PlayerData.GetPlayerName()}: {m_Points}";
     }
 
     public void GameOver()
